6ffa3538e0d28ed765089afa4b3ad7d452d15e2f
jeff <1411008062@qq.com>
2019-10-07 22:22:00 +0800

    add

---
+++ pages/main/main.js
@@ -0,0 +1,90 @@
+// pages/main/main.js
+const app = getApp()
+
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    nowPage: "homePage",
+    nowIndex: 0,
+    tabBar: [{
+        iconClass: "iconfont icon-shouye",
+        text: "主页",
+        tapFunction: "toHomePage",
+        active: "active"
+      },
+      {
+        iconClass: "iconfont icon-wode",
+        text: "活动",
+        tapFunction: "toActivityPage",
+        active: ""
+      },
+      {
+        iconClass: "iconfont icon-wode",
+        text: "我的",
+        tapFunction: "toMyPage",
+        active: ""
+      }
+    ]
+  },
+
+  toHomePage() {
+    this.setData({
+      nowPage: "homePage",
+      nowIndex: 0
+    });
+  },
+  toActivityPage() {
+    this.setData({
+      nowPage: "activityPage",
+      nowIndex: 1
+    });
+  },
+  toMyPage() {
+    this.setData({
+      nowPage: "myPage",
+      nowIndex: 2
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {},
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {},
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {},
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {}
+});
